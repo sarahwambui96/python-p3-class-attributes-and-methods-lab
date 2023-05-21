919a0f8e55ed92a5bc46247c8b850d6bcf8d152c
sarahwambui96 <sarahwambui843@gmail.com>
2023-05-21 17:58:28 +0300

    Completed assignment

--- lib/song.py
+++ lib/song.py
@@ -8,7 +8,7 @@ class Song:
         self.name = name
         self.artist = artist
         self.genre = genre
-        Song.add_song_to_count()
+        self.add_song_to_count()
         self.add_to_genres()
         self.add_to_artists()
         self.add_to_genre_count()
